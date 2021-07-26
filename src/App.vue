b13c9874c84593ccebd816d7156d088732845cec
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-07-26 14:28:58 +0800

    chore: initialize project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+  import { defineComponent, reactive, onMounted } from "vue";
+  import HelloWorld from "./components/HelloWorld.vue";
+
+  export default defineComponent({
+    name: "App",
+    components: {
+      HelloWorld,
+    }
+  });
+</script>
+
+<style>
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
